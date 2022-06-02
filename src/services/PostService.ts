387786e0c78b5960fc92650495d6f20f125154ce
WalmirLucena <walmirlucena3@gmail.com>
2022-06-02 08:44:54 -0400

    feat: add Post service

---
+++ src/services/PostService.ts
@@ -0,0 +1,40 @@
+import { ZodError } from 'zod';
+import { Post, PostSchema } from '../interfaces/PostInterface';
+import PostModel from '../models/PostModel';
+
+export interface ServiceError {
+    error: ZodError;
+}
+
+class PostService {
+  constructor(public model = new PostModel()) {}
+
+  public async read(): Promise<Post[]> {
+    const items = await this.model.read();
+    return items;
+  }
+
+  public async create(obj: Post): Promise<Post | null | ServiceError> {
+    const parsed = PostSchema.safeParse(obj);
+
+    if (!parsed.success) {
+      return { error: parsed.error };
+    }
+
+    const item = await this.model.create(obj);
+
+    return item;
+  }
+
+  public async readOne(id: string): Promise<Post | null | ServiceError> {
+    const item = await this.model.readOne(id);
+    return item;
+  }
+
+  public async delete(id: string): Promise<Post | null | ServiceError> {
+    const item = await this.model.delete(id);
+    return item;
+  }
+}
+
+export default PostService;
