f137833708e691c466f8fdd0053a490d7b5eefa0
WalmirLucena <walmirlucena3@gmail.com>
2022-06-02 08:00:13 -0400

    feat: add morgan to generate log

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express from 'express';
+import express, { Router } from 'express';
+import connectToDatabase from './models/connection';
 import cors from 'cors';
+import morgan from 'morgan';
 
 class App {
   public app: express.Application;
@@ -8,11 +10,17 @@ class App {
     this.app = express();
     this.app.use(express.json());
     this.app.use(cors());
+    this.app.use(morgan('dev'));
     
   }
 
   public startServer(PORT: String | number = 3001): void {
+    connectToDatabase();
     this.app.listen(PORT, () => console.log(`Server running here http://localhost:${PORT}`));
+  }
+
+  public addRouter(router: Router){
+      this.app.use(router);
   }
 
  
