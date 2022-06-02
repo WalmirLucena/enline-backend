3ce463968a36a8235ef02c2efecb1c2864a95b2e
WalmirLucena <walmirlucena3@gmail.com>
2022-06-02 10:05:53 -0400

    feat: config server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import App from './app';
 
 import 'dotenv/config';
+import MainRouter from './router/Router';
+import PostController from './controller/PostController';
 
 const server = new App();
+
+const postRoute = new MainRouter();
+const post = new PostController();
+
+postRoute.addRoute(post);
+server.addRouter(postRoute.router);
 
 server.startServer(process.env.PORT);
 
