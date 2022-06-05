47bfce57e33791c3eb633a2ad816ba1f25e197c5
WalmirLucena <walmirlucena3@gmail.com>
2022-06-05 19:17:27 -0400

    test: add file mock

---
+++ src/tests/mocks/fileMock.ts
@@ -0,0 +1,19 @@
+export const fileMock = {
+  name: 'iphone.jpg',
+  size: 69372,
+  key: '0508825030c8edadf475b3b035487678-iphone.jpg',
+  url: 'http://localhost:3005/files/0508825030c8edadf475b3b035487678-iphone.jpg',
+  _id: '629b819e3cba2ef42c89e232',
+  createdAt: '2022-06-04T16:00:30.897Z',
+  __v: 0,
+};
+
+export const fileMockList = [{
+  name: 'iphone.jpg',
+  size: 69372,
+  key: '0508825030c8edadf475b3b035487678-iphone.jpg',
+  url: 'http://localhost:3005/files/0508825030c8edadf475b3b035487678-iphone.jpg',
+  _id: '629b819e3cba2ef42c89e232',
+  createdAt: '2022-06-04T16:00:30.897Z',
+  __v: 0,
+}];
